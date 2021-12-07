672b6316b603e5feef1077ec4bd4f7a4ea31a9b4
Shamzi <shamzi.erz@hotmail.com>
2021-12-07 12:39:42 +0400

    Update image model

--- Pixabay/Domain/Entities/ImagesModel.swift
+++ Pixabay/Domain/Entities/ImagesModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct ImagesModel {
+
+    let id: Int?
+    let pageURL: String?
+    let type: String?
+    let tags: String?
+    let previewURL: String?
+    let previewWidth: Int?
+    let previewHeight: Int?
+    let webformatURL: String?
+    let webformatWidth: Int?
+    let webformatHeight: Int?
+    let largeImageURL: String?
+    let imageWidth: Int?
+    let imageHeight: Int?
+    let imageSize: Int?
+    let views: Int?
+    let downloads: Int?
+    let collections: Int?
+    let likes: Int?
+    let comments: Int?
+    let user_id: Int?
+    let user: String?
+    let userImageURL: String?
+}
