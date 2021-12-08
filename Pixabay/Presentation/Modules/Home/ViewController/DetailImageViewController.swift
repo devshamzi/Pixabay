9667d80d754a9ec81c8c9f021a9be73ee9abc37b
Shamzi <shamzi.erz@hotmail.com>
2021-12-08 13:56:52 +0400

    Add: Detail image

---
+++ Pixabay/Presentation/Modules/Home/ViewController/DetailImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailImageViewController.swift
+//  Pixabay
+//
+//  Created by Shamzi on 08/12/2021.
+//
+
+import UIKit
+
+class DetailImageViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
