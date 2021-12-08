b51d1bf32f1bf2680c85b24e1727c52c1d9b4895
Shamzi <shamzi.erz@hotmail.com>
2021-12-08 16:32:48 +0400

    Added more UI info

--- Pixabay/Presentation/Modules/Home/ViewController/DetailImageViewController.swift
+++ Pixabay/Presentation/Modules/Home/ViewController/DetailImageViewController.swift
@@ -18,7 +18,10 @@ class DetailImageViewController: BaseViewController {
     @IBOutlet weak var closeBtn: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
-
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var downloadLabel: UILabel!
+    @IBOutlet weak var viewLabel: UILabel!
 
     // MARK: - Variables
     var viewModel = DetailImageViewModel()
@@ -36,6 +39,12 @@ class DetailImageViewController: BaseViewController {
             self.imageView.sd_setImage(with: URL(string: (imageUrl)), placeholderImage: UIImage(named: "placeholder.png"))
             self.profileImage.sd_setImage(with: URL(string: (userImageUrl)), placeholderImage: UIImage(named: "placeholder.png"))
             self.userNameLabel.text = self.viewModel.dataSource?.user
+
+            self.likeLabel.text = "\(self.viewModel.dataSource?.likes ?? 0)"
+            self.commentLabel.text = "\(self.viewModel.dataSource?.comments ?? 0)"
+            self.downloadLabel.text = "\(self.viewModel.dataSource?.downloads ?? 0)"
+            self.viewLabel.text = "\(self.viewModel.dataSource?.views ?? 0)"
+
         }
 
     }
